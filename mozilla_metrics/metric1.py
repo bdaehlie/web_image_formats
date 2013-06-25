f7788a963aab91fbf09d89a7295db71fa4e167d7
Josh Aas <joshmoz@gmail.com>
2013-06-24 22:17:34 -0500

    Update to cleaner (if possibly less efficient) code, interpolate values.

--- mozilla_metrics/metric1.py
+++ mozilla_metrics/metric1.py
@@ -6,6 +6,7 @@
 #  Software License Agreement:  http://www.webmproject.org/license/software/
 #  Additional IP Rights Grant:  http://www.webmproject.org/license/additional/
 # -----------------------------------------------------------------------------
+# Modified by Josh Aas, Mozilla Corporation
 
 import os
 import sys
@@ -20,7 +21,7 @@ convert = "/opt/local/bin/convert"
 
 tmpdir = "/tmp/"
 
-# Return a single float value from the ssim program output
+# Return a single float value from the ssim program output.
 def ssim_float_for_images(img1_path, img2_path):
   cmd = "%s %s %s" % (ssim, img1_path, img2_path)
   proc = os.popen(cmd, "r")
@@ -30,95 +31,112 @@ def ssim_float_for_images(img1_path, img2_path):
   b = float(proc.readline().strip().strip('%'))
   return (((r + g + b) / 3) / 100)
 
-# Returns an SSIM value for a WebP image compared to a PNG
-def get_webp_ssim(png_path, quality, webp_path):
-  cmd = "%s -quiet -q %d %s -o %s" % (cwebp, quality, png_path, webp_path)
-  os.system(cmd)
-  webp_png_path = webp_path + ".png"
-  cmd = "%s %s -o %s" % (dwebp, webp_path, webp_png_path)
-  os.system(cmd)
-  result = ssim_float_for_images(png_path, webp_png_path)
-  os.remove(webp_png_path)
-  return result
-
-# Find the closest WebP ssim match for a given JPEG
-def get_webp_ssim_match(j_ssim, png_path, webp_path):
-  q = 95
-  curr_ssim = get_webp_ssim(png_path, q, webp_path)
-  w_ssim = curr_ssim
-
-  delta_q = int(1000 * (j_ssim - curr_ssim))
-  if q + delta_q > 100:
-    delta_q = 100 - q
-  elif q + delta_q < 0:
-    delta_q = 1 - q
-
-  test_q = q + delta_q
-  while delta_q != 0:
-    accept = False
-    if test_q > 0 and test_q <= 100:
-      curr_ssim = get_webp_ssim(png_path, test_q, webp_path)
-      # accept if curr_ssim is in the same side of j_ssim.
-      accept = (curr_ssim < j_ssim) ^ (delta_q < 0)
-
-    if accept:
-      q = test_q
-      w_ssim = curr_ssim
-    else:
-      delta_q = int(float(delta_q) / 2)
-
-    test_q = q + delta_q
-
 def main(argv):
   png = argv[1]
   filename = os.path.basename(png)
   basename = tmpdir + os.path.splitext(filename)[0]
 
-  jpeg_files = []
-  webp_files = []
-
+  # Quality values we're interested in. This is essentially an optimization
+  # to avoid working on every one.
   quality_values = [50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
-  for q in quality_values:
-    ppm_file =  basename + str(q) + ".ppm"
-    jpg_file =  basename + str(q) + ".jpg"
-    webp_file = basename + str(q) + ".webp"
-    jpg_ppm =   basename + str(q) + "_jpg.ppm"
-    jpg_png =   basename + str(q) + "_jpg.png"
 
-    cmd = "%s %s %s" % (convert, png, ppm_file)
+  # These lists will have one entry per quality value we're testing.
+  jpeg_ssim_values = []
+  jpeg_file_sizes = []
+
+  # Each list will be 100 entries long, one for each quality option.
+  webp_ssim_values = []
+  webp_file_sizes = []
+
+
+  # Calculate SSIM and file size for all JPEG quality levels.
+  for q in quality_values:
+    ppm =     basename + str(q) + ".ppm"
+    jpg =     basename + str(q) + ".jpg"
+    jpg_ppm = basename + str(q) + "_jpg.ppm"
+    jpg_png = basename + str(q) + "_jpg.png"
+
+    cmd = "%s %s %s" % (convert, png, ppm)
     os.system(cmd)
-    cmd = "%s -quality %d -outfile %s %s" % (cjpeg, q, jpg_file, ppm_file)
+    cmd = "%s -quality %d -outfile %s %s" % (cjpeg, q, jpg, ppm)
     os.system(cmd)
-    cmd = "%s -outfile %s %s" % (djpeg, jpg_ppm, jpg_file)
+    cmd = "%s -outfile %s %s" % (djpeg, jpg_ppm, jpg)
     os.system(cmd)
     cmd = "%s %s %s" % (convert, jpg_ppm, jpg_png)
     os.system(cmd)
 
-    j_ssim = ssim_float_for_images(png, jpg_png)
+    jpeg_ssim_values.append(ssim_float_for_images(png, jpg_png))
+    jpeg_file_sizes.append(os.path.getsize(jpg))
 
-    os.remove(ppm_file)
+    os.remove(ppm)
+    os.remove(jpg)
     os.remove(jpg_ppm)
     os.remove(jpg_png)
 
-    jpeg_files.append(jpg_file)
+  # Calculate SSIM and file size for all WebP quality levels.
+  i = 0
+  while i < 100:
+    q = i + 1
 
-    get_webp_ssim_match(j_ssim, png, webp_file)
-    webp_files.append(webp_file)
+    webp =     basename + str(q) + ".webp"
+    webp_png = basename + str(q) + "_webp.png"
 
-  jpeg_total_size = 0;
-  for f in jpeg_files:
-    jpeg_total_size += os.path.getsize(f)
+    cmd = "%s -quiet -q %d %s -o %s" % (cwebp, q, png, webp)
+    os.system(cmd)
+    cmd = "%s %s -o %s" % (dwebp, webp, webp_png)
+    os.system(cmd)
 
-  webp_total_size = 0;
-  for f in webp_files:
-    webp_total_size += os.path.getsize(f)
+    webp_ssim_values.append(ssim_float_for_images(png, webp_png))
+    webp_file_sizes.append(os.path.getsize(webp))
 
-  print "WebP size compared to JPEG: " + str(float(webp_total_size) / float(jpeg_total_size));
+    os.remove(webp)
+    os.remove(webp_png)
 
-  for f in jpeg_files:
-    os.remove(f)
-  for f in webp_files:
-    os.remove(f)
+    i += 1
+
+  # For each quality value we're interested in, calculate the size of a
+  # WebP file that is equivalent to the JPEG file via interpolation.
+  webp_ssim_equiv_file_sizes = []
+  for s in jpeg_ssim_values:
+    i = 0
+    while i < 100:
+      if webp_ssim_values[i] < s:
+        if i == 99:
+          webp_ssim_equiv_file_sizes.append(webp_file_sizes[99])
+          break;
+      if webp_ssim_values[i] == s:
+        webp_ssim_equiv_file_sizes.append(webp_file_sizes[i])
+        break
+      if webp_ssim_values[i] > s:
+        if i == 0:
+          webp_ssim_equiv_file_sizes.append(webp_file_sizes[0])
+          break
+        webp_diff = webp_ssim_values[i] - webp_ssim_values[i - 1]
+        if webp_diff == 0:
+          webp_ssim_equiv_file_sizes.append(webp_file_sizes[i])
+          break;
+        jpeg_diff = s - webp_ssim_values[i - 1]
+        percent = jpeg_diff / webp_diff
+        webp_fs_diff = webp_file_sizes[i] - webp_file_sizes[i - 1]
+        interpolated = (webp_fs_diff * percent) + webp_file_sizes[i - 1]
+        # print "JPEG SSIM: " + str(s)
+        # print "WebP SSIM larger: " + str(webp_ssim_values[i]) + " WebP SSIM smaller: " + str(webp_ssim_values[i - 1])
+        # print "WebP file size larger: " + str(webp_file_sizes[i]) + " WebP file size smaller: " + str(webp_file_sizes[i - 1])
+        # print "Interpolated file size: " + str(interpolated)
+        webp_ssim_equiv_file_sizes.append(interpolated)
+        break;
+
+      i += 1
+
+  jpeg_total_size = 0
+  for fs in jpeg_file_sizes:
+    jpeg_total_size += fs
+
+  webp_total_size = 0
+  for fs in webp_ssim_equiv_file_sizes:
+    webp_total_size += fs
+
+  print "File size change from JPEG to WebP at equivalent SSIM: " + str(float(webp_total_size) / float(jpeg_total_size))[:5]
 
 if __name__ == "__main__":
   main(sys.argv)
