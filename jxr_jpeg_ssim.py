6dea1ba8596f1661943d71fffa264bc53e8edc75
Josh Aas <joshmoz@gmail.com>
2013-07-13 13:29:37 -0500

    Binary search for JPEG-XR.

--- jxr_jpeg_ssim.py
+++ jxr_jpeg_ssim.py
@@ -5,68 +5,78 @@
 import os
 import sys
 import time
+import math
 import test_utils
+
+# Returns an SSIM value and a file size
+def get_jxr_results(in_png, jxr_q):
+  tmp_file_base = test_utils.path_for_file_in_tmp(in_png)
+  jxr = tmp_file_base + str(jxr_q) + ".jxr"
+  test_utils.png_to_jxr(in_png, jxr_q, jxr)
+  jxr_png = jxr + ".png"
+  test_utils.jxr_to_png(jxr, jxr_png)
+  ssim = test_utils.ssim_float_for_images(in_png, jxr_png)
+  file_size = os.path.getsize(jxr)
+  os.remove(jxr)
+  os.remove(jxr_png)
+  return (ssim, file_size)
 
 def main(argv):
   if len(argv) < 2:
-  	print "First arg is a JPEG quality value to test (e.g. '75')."
-  	print "Second arg is the path to an image to test (e.g. 'images/Lenna.jpg')."
-  	print "Output is four lines: SSIM, JPEG-XR file size, JPEG file size, and JPEG-XR to JPEG file size ratio."
-  	print "Output labels have no spaces so that a string split on a line produces the numeric result at index 1."
-  	return
+    print "First arg is a JPEG quality value to test (e.g. '75')."
+    print "Second arg is the path to an image to test (e.g. 'images/Lenna.jpg')."
+    print "Output is four lines: SSIM, JPEG-XR file size, JPEG file size, and JPEG-XR to JPEG file size ratio."
+    print "Output labels have no spaces so that a string split on a line produces the numeric result at index 1."
+    return
 
   jpeg_q = int(argv[1])
   png = argv[2]
 
-  tmp_file_base = test_utils.path_for_file_in_tmp(png)
+  jpeg_results = test_utils.get_jpeg_results(png, jpeg_q)
+  jpeg_ssim = jpeg_results[0]
+  jpeg_file_size = jpeg_results[1]
 
-  jpg = tmp_file_base + str(jpeg_q) + ".jpg"
-  test_utils.png_to_jpeg(png, jpeg_q, jpg)
-  jpeg_file_size = os.path.getsize(jpg)
-  jpg_png = jpg + ".png"
-  test_utils.jpeg_to_png(jpg, jpg_png)
-  jpeg_ssim = test_utils.ssim_float_for_images(png, jpg_png)
-  os.remove(jpg)
-  os.remove(jpg_png)
+  # Possible quality values so we can use an algorithm implementation that
+  # only needs to deal with integer array indices.
+  possible_q = []
+  q = 0.0
+  while q < 1.0:
+    possible_q.append(q)
+    q += 0.01
 
-  jxr_ssim = 0.0
-  jxr_file_size = 0
-  q = 0.99
-  while q >= 0.0:
-    jxr = tmp_file_base + str(q) + ".jxr"
-    test_utils.png_to_jxr(png, q, jxr)
-    jxr_png = jxr + ".png"
-    test_utils.jxr_to_png(jxr, jxr_png)
-    ssim = test_utils.ssim_float_for_images(png, jxr_png)
-    file_size = os.path.getsize(jxr)
-    os.remove(jxr)
-    os.remove(jxr_png)
-    if ssim < jpeg_ssim:
-      if jxr_file_size == 0:
-        # Normally we require that the target format be capable of producing an
-        # image at equal or greater quality than JPEG image being tested.
-        # However, sometimes the quality metric will produce nearly equal values at
-        # the high end and the target format can't match JPEG, particularly if
-        # the image lacks much complexity. It can also happen, usually with the same
-        # types of images, that the quality metric does not go down as requested
-        # quality goes down. This can be a bug in the encoder or the quality metric.
-        # In these troublesome cases, if the quality metric is close enough to JPEG
-        # we'll simply charge the target image format with the highest file size that
-        # it produces.
-        if (jpeg_ssim - ssim) < 0.001:
-          jxr_ssim = ssim
-          jxr_file_size = file_size
-        else:
-          sys.stderr.write("Target format cannot match JPEG quality, aborting!\n")
-          sys.exit(1)
-      else:
-        jxr_file_size = test_utils.file_size_interpolate(jxr_ssim, ssim, jpeg_ssim, jxr_file_size, file_size)
-        # Note that jpeg_ssim must be updated *after* it's used for interpolation.
-        jxr_ssim = jpeg_ssim # The JPEG-XR SSIM after interpolation is the same as the JPEG SSIM.
-      break
-    jxr_ssim = ssim
-    jxr_file_size = file_size
-    q -= 0.01
+  low_index = -1
+  low_results = (0.0, 0)
+  high_index = len(possible_q)
+  high_results = (0.0, 0)
+  while (high_index - low_index) > 1:
+    i = int(math.floor((float(high_index - low_index) / 2.0)) + low_index)
+    jxr_results = get_jxr_results(png, possible_q[i])
+    jxr_ssim = jxr_results[0]
+    jxr_file_size = jxr_results[1]
+    if jxr_ssim == jpeg_ssim:
+      low_index = high_index = i
+      low_results = high_results = jxr_results
+      break;
+    if jxr_ssim < jpeg_ssim:
+      low_index = i
+      low_results = jxr_results
+    if jxr_ssim > jpeg_ssim:
+      high_index = i
+      high_results = jxr_results
+
+  # See 'hevc_tecnick_034_situation.txt' for explanation of the
+  # following code.
+  if low_index == -1 or high_index == len(possible_q):
+    if low_index == -1 and (high_results[0] - jpeg_ssim) < 0.001:
+      low_results = high_results
+    elif high_index == len(possible_q) and (jpeg_ssim - low_results[0]) < 0.001:
+      high_results = low_results
+    else:
+      sys.stderr.write("Failure: Unsuccessful binary search!\n")
+      sys.exit(1)
+
+  # Calculate file size via interpolation.
+  jxr_file_size = test_utils.file_size_interpolate(high_results[0], low_results[0], jpeg_ssim, high_results[1], low_results[1])
 
   ratio = float(jxr_file_size) / float(jpeg_file_size)
 
